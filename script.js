f0fa4483d02ef07b4d6acc1c7bae4cfd7ec25d31
unknown <avital_boom@hotmail.com>
2018-08-02 15:27:39 +0300

    better look, css, spaces

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 beers = [];
 
-
 function addBeer(name, category, rating) {
     beers.push(
         {
@@ -9,13 +8,11 @@ function addBeer(name, category, rating) {
             rating: rating
         }
     );
-
 }
-
 
 $('.post-beer').on('click', function () {
     var beerName = $('.beer-input').val();
-    
+
     var beerCategory = $('.category-input').val();
     var beerRating = $('.rating-input').val();
     addBeer(beerName, beerCategory, beerRating);
@@ -23,40 +20,11 @@ $('.post-beer').on('click', function () {
 });
 
 function renderBeers() {
-    // $("p").html("Hello <b>world</b>!");
-
     $('.beers-list').empty();
     var i;
     for (i = 0; i < beers.length; i++) {
-
         console.log(beers[i]);
-        $('.beers-list').append('<div>' + beers[i].name + " , " + 
-        beers[i].category + " , " + beers[i].rating + '</div>');
-
-
+        $('.beers-list').append('<div>' + beers[i].name + " , " +
+            beers[i].category + " , " + beers[i].rating + '</div>');
     }
 }
-
-
-
-
-
-
-
-//     const newBeer = $("<li>" + $('.beer-input').val() + "</li>");
-//     $(".beer-list").append(newBeer);
-//   });
-
-
-
-//   $('.beer-list').append('<li>' + 
-//   $('.beer-input').val() + $('.category-input').val() + '</li>'
-// )
-
-  ///
-//   var clicked = function () {
-//     alert('clicked!');
-//   }
-
-//   $('button').click(clicked);
-
